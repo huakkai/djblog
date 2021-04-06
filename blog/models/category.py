ed1cbeb10de689243f493d30de60ca464d3c884b
huakkai <huaqiangyan@163.com>
2021-04-06 17:05:42 +0800

    徽章Badges

--- blog/models/category.py
+++ blog/models/category.py
@@ -2,7 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
+    STYLE_CHOICES=[
+        ('badge-default', 'badge-default'),
+        ('badge-primary', 'badge-primary'),
+        ('badge-success', 'badge-success'),
+        ('badge-info', 'badge-info'),
+        ('badge-warning', 'badge-warning'),
+        ('badge-danger', 'badge-danger'),
+    ]
     name = models.CharField('Blog Category', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='badge-default', max_length=100, verbose_name='Style')
     index = models.IntegerField(default=1, verbose_name='Sequence')
 
     class Meta:
